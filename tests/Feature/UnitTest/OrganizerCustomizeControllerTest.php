c5261d20b472fb70cb8533f91fe94606608b2115
GeovanneAtanazio <geovanne.atanazio@dcomp.ufs.br>
2023-06-21 07:19:22 -0300

    Teste da PageDesign

--- tests/Feature/UnitTest/OrganizerCustomizeControllerTest.php
+++ tests/Feature/UnitTest/OrganizerCustomizeControllerTest.php
@@ -27,4 +27,14 @@ class OrganizerCustomizeControllerTest extends TestCase
         ['name' => 'Empresa', 'email' => 'empresa@teste.com', 'enable_organiser_page' => '1']);
         $this->assertEquals('success', $response->decodeResponseJson()['status']);
     }
+
+    /**
+     * @test
+     */
+    public function testPostEditOrganiserPageDesign()
+    {
+        $response = $this->post(route('postEditOrganiserPageDesign', ['organiser_id' => '1']),
+        ['page_header_bg_color' => '#c22a1e', 'page_text_color' => '#ffffff', 'page_bg_color' => '#000000']);
+        $this->assertEquals('success', $response->decodeResponseJson()['status']);
+    }
 }
